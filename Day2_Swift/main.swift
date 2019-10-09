ece0138315c2d6a441d7f4f2e2d16bf7b8bffcae
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-09 18:42:22 -0400

    first commit

--- Day2_Swift/main.swift
+++ Day2_Swift/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+var a : [ Int]
+a = [Int]()
+
+a.append(10)
+a.append(10)
+a.append(10)
+a.append(10)
+a.append(10)
+
+for i in a {
+    print(i)
+}
+
+print(a.count)
+print(a.capacity)
+
+var b = Array.init(repeating: 0, count: 5)
+
+a = a + b
+
+for i in a{
+    print(i)
+}
+
+
 
